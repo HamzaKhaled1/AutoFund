f2729d6e7436a7f18b3759b3ab9725dae13b3980
HamzaKhaled1 <hamza.khaled2091@gmail.com>
2024-11-03 15:50:20 +0200

    fix

--- src/Componants/Circle.jsx
+++ src/Componants/Circle.jsx
@@ -6,7 +6,7 @@ const Circle=({text,img,id})=>{
     const handleClick=()=>{setFlag(!flag)}
     const [data,setData]=useState(text2.text.find(item=>item.id==id))
     return(<> 
-    <div className={`sm:h-72  sm:w-72 flex h-40 w-40  justify-center mt-20 items-center  relative border-2 border-black bg-cover transition-transform to-sky-300 hover:scale-105 duration-300 cursor-pointer ease-in-out ${flag?"rounded-xl h-52 w-48 sm:h-72 sm:w-72":"rounded-full"} `}
+    <div className={`sm:h-72  sm:w-72 flex h-40 w-40  justify-center mt-20 items-center  relative border-2 border-black bg-cover transition-transform to-sky-300 hover:scale-105 duration-300 cursor-pointer ease-in-out ${flag?"rounded-xl h-64 w-64 sm:h-72 sm:w-72":"rounded-full"} `}
     style={{backgroundImage:`url('${img}')`}} onClick={handleClick}>
         
         <p className={` font-bold text-white bg-black/50  text-xs  sm:text-base  flex text-center justify-center ${flag?"text-lg p-2 absolute bottom-0 rounded-none":"text-2xl p-2 rounded-3xl"}`}>
